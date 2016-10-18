66f469a62a746d4914afc8d60d9bb583c172e7f2
Ronald Pereira <ronald.drp11@gmail.com>
2016-10-18 14:47:55 -0200

    Linker com abertura de input para todos os argumentos

--- code/linker/linker.c
+++ code/linker/linker.c
@@ -4,7 +4,14 @@
 
 int main(int argc, char* argv[])
 {
-	/* code */
+	FILE *output, *_main, *library[argc-2];
+	int i;
+
+	output = fopen(argv[1], "w+r"); // Abertura do arquivo de saida
+	_main = fopen(argv[2], "w+r"); // Abertura do arquivo principal (main)
+
+	for (i = 2; i < argc; i++)
+		library[i-2] = fopen(argv[i], "w+r"); // Abertura por meio de uma iteração de todos os arquivos de funções externas (libraries)
 
 	return 0;
 }
